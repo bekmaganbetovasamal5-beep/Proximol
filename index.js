23e84b937e280285a568da0daef72180e795ee04
amanvelikiigmailcom <aman.velikii@gmail.com>
2025-10-09 19:27:19 +0500

    1 тестовая версия

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("✅ Backend работает внутри Docker!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Сервер запущен на порту ${PORT}`);
+});
